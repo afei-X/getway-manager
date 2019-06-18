d7ae06c0fa815fcf7846360f8b339420be8b5ded
cygr-0101-01-0161 <gongxiangfei@jingdata.com>
2019-06-18 22:03:03 +0800

    getway

---
+++ src/main/java/com/hshc/getwaymanage/LeadsGetwayManageApplication.java
@@ -0,0 +1,12 @@
+package com.hshc.getwaymanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadsGetwayManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadsGetwayManageApplication.class, args);
+	}
+}
